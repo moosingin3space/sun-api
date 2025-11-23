0728df2512ba3ec0dbb4eb5acfc76c3251cc9fcc
Nathan Moos <moosingin3space@gmail.com>
2025-11-23 00:29:47 -0800

    feat: add sun api cloudflare worker

---
+++ sunapi-cf/src/lib.rs
@@ -0,0 +1,29 @@
+use sunapi::Platform;
+use tower_service::Service;
+use worker::*;
+
+struct CloudflarePlatform;
+
+impl Platform for CloudflarePlatform {
+    fn now(&self) -> jiff::Timestamp {
+        jiff::Timestamp::now()
+    }
+
+    fn schedule_webhook_at<B>(&self, _when: jiff::Timestamp, _req: axum::http::Request<B>)
+    where
+        B: axum::body::HttpBody,
+    {
+        todo!()
+    }
+}
+
+#[event(fetch)]
+async fn fetch(
+    req: HttpRequest,
+    _env: Env,
+    _ctx: Context,
+) -> Result<axum::http::Response<axum::body::Body>> {
+    let plat = CloudflarePlatform;
+    let mut router = sunapi::create_router(plat);
+    Ok(router.call(req).await?)
+}
