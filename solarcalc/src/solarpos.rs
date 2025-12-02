7ed2dc73ebb8c26f75113dc02e3f766c33374d4d
Nathan Moos <moosingin3space@gmail.com>
2025-12-01 21:51:14 -0800

    fix: formatting

--- solarcalc/src/solarpos.rs
+++ solarcalc/src/solarpos.rs
@@ -53,12 +53,18 @@ mod tests {
 
     #[test]
     fn equation_of_time() {
-        check(jiff::civil::date(2025, 11, 19), expect![[r#"
+        check(
+            jiff::civil::date(2025, 11, 19),
+            expect![[r#"
             14.312655254932228
-        "#]]);
-        check(jiff::civil::date(2025, 7, 4), expect![[r#"
+        "#]],
+        );
+        check(
+            jiff::civil::date(2025, 7, 4),
+            expect![[r#"
             -4.049227066974504
-        "#]]);
+        "#]],
+        );
     }
 
     #[track_caller]
